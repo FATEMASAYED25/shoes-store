90b63f46260784b666168b7c334fb7cb6f4d9d28
wewe15 <ahm.sami.271@gmail.com>
2022-06-12 12:04:37 +0200

    Apply payment on orders.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,10 +3,13 @@ const userRoutes = require('./user');
 const categoryRoutes = require('./category');
 const productRoutes = require('./product');
 const orderRoutes = require('./order');
+const paymentRoute = require('./payment');
+
 
 router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/orders', orderRoutes);
+router.use('/payment', paymentRoute);
 
 module.exports = router;
