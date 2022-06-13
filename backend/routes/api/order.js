8e6d079c004d13e2163004b2852ab33a3d30ddac
wewe15 <ahm.sami.271@gmail.com>
2022-06-13 12:31:05 +0200

    added payment for order.

--- backend/routes/api/order.js
+++ backend/routes/api/order.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Order, Order_item, Product } = require('../../models');
 const { authAdmin, authUser } = require('../../middlewares/authentication');
+const { createCharge, createToken } = require('../../middlewares/payment');
 
 
 router.get('/', authAdmin, async (_req, res) => {
@@ -63,6 +64,48 @@ router.post('/', authUser, async (req, res) => {
         res.status(500).json({ error: 'Something went wrong' })
     }
 
+});
+
+router.put('/:id/payment', async (req, res) => {
+    try {
+        let order = await Order.findByPk(req.params.id,{
+            include: ['products']
+        });
+        if(!order){
+            return res.json("Order Not Found")
+        }
+        let price = 0;
+        for(let i =0; i<order.products.length; i++){
+            price += parseInt(order.products[i].total_price)
+        }
+        const card_data = {
+            fullName: req.body.fullName,
+            cardNumber: req.body.cardNumber,
+            month: req.body.month,
+            year: req.body.year,
+            cvv: req.body.cvv
+        } 
+        const token = await createToken(card_data);
+        if (token.error) {
+            return res.status(500).json("please try again");
+        }
+        if (!token.id) {
+            return res.status(404).json("payment failed2");
+        }
+        const charge = await createCharge(token.id, price*100);
+        console.log(charge)
+        if (charge && charge.status === 'succeeded') { 
+            order.is_paid = true;
+            order.paid_at = Date.now();
+            order = await order.save();
+            return res.status(201).json({ message: 'Order Paid', order})
+        }else {
+            return res.status(404).json("payment failed");
+        }
+    }catch (err) {
+        console.log(err)
+        res.status(500).json({ error: 'Something went wrong' })
+    }
 });
 
 router.post('/:id/products', authUser, async (req, res) => {
